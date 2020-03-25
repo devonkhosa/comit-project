138f2350e48d3f1cb63a9fc28ce722df5a82cbaf
devonkhosa <dalvinderkhosa@gmail.com>
2020-03-24 22:47:20 -0700

    Made changes

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,10 +1,13 @@
-var app = require('express')();
-var http = require('http').createServer(app);
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const port = 3000;
 
-app.get('/', function(req, res){
+app.get('/', (req, res)=>{
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+server.listen(port, ()=>{
+  console.log(`Server is available on localhost:${port}`);
 });

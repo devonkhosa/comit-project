10e57a8ca1ace554dce62a4a92a4c311d7ece30e
devonkhosa <dalvinderkhosa@gmail.com>
2020-03-27 22:23:07 -0700

    Added npm colors

--- chat-app/index.js
+++ chat-app/index.js
@@ -4,6 +4,10 @@ const http = require('http');
 const server = http.createServer(app);
 const port = 3000;
 
+//Extra; provides colour to the console.log output.
+const colors = require('colors');
+
+
 app.get('/', (req, res)=>{
   res.send('<h1>Hello world</h1>');
 });
@@ -12,5 +16,5 @@ server.listen(port, (err)=>{
   if (err) {
     return console.log(`Error! Something went wrong.`)
   }
-  console.log(` is available on localhost: ${port}.`);
+  console.log('Chat is online:', ('=====>'.rainbow), (`localhost:${port}/`.underline.cyan), ('<====='.rainbow));
 });

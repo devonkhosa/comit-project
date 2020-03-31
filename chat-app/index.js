b2eb5ea7eb1c94e98cdc156b363f36e65bf44d0f
devonkhosa <dalvinderkhosa@gmail.com>
2020-03-30 18:30:21 -0700

    Cleaned up index.js

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,50 +1,58 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const http = require('http').createServer(app);
 const port = 3000;
-const io = require('socket.io')(http);
+const io = require('socket.io');
+const socket = io(http);
 const pug = require('pug');
 
-//pug compile sourcecode <--
+//middleware
 app.set('view engine', 'pug')
-
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', (req, res)=> {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('home');
 })
 
-//Extra; provides colour to the console.log output.
-const colors = require('colors');
+//modules
+const generalChat = require('./routes/generalChat');
+const home = require('./routes/home');
+const nodebcChat = require('./routes/nodebcChat');
+const randomChat = require('./routes/randomChat');
+const about = require('./routes/about');
 
-// __dirname allows you to get the pwd
-app.get('/', (req, res)=>{
-  res.sendFile(__dirname + '/main/index.pug');
-});
+app.get('/general', generalChat);
+app.get('/home', home);
+app.get('/node', nodebcChat);
+app.get('/random', randomChat);
+app.get('/about', about);
 
 //socket.io -> user connection console log
-io.on('connection', (socket)=>{
+socket.on('connection', (socket)=>{
   console.log('a user', ('connected'.green));
   socket.on('disconnect', (socket)=>{
     console.log('a user', ('disconnected'.red));
   });
 }); 
-
 //socket.io -> message console log
-io.on('connection', (socket)=>{
+socket.on('connection', (socket)=>{
   socket.on('chat message', (msg)=>{
     console.log(('Message: '.blue) + msg.grey);
   })
 })
-
 //displays messages
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+socket.on('connection', (socket)=>{
+  socket.on('chat message', (msg)=>{
+    socket.emit('chat message', msg);
   });
 });
 
 http.listen(port, (err)=>{
+  require('colors'); //Extra; provides colour to the console.log output.
   if (err) {
-    return console.log(`Error! Something went wrong.`.bgRed)
+    return console.log(('Error! Something went wrong.').bgRed);
   }
   console.log('Chat is online:', ('=====>'.rainbow), (`http://localhost:${port}/`.underline.cyan), ('<====='.rainbow));
 });

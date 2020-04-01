eff280330b6ec8d6cf72784cc9dd635132bc8fe0
devonkhosa <dalvinderkhosa@gmail.com>
2020-04-01 11:17:35 -0700

    Added mongoose

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 const http = require('http').createServer(app);
 const port = 3000;
 const io = require('socket.io')(http);
 const pug = require('pug');
+
+//Mongoose
+const dbUrl = 'mongodb://devonkhosa:megaman12@nodebc-shard-00-00-c6sqh.mongodb.net:27017,nodebc-shard-00-01-c6sqh.mongodb.net:27017,nodebc-shard-00-02-c6sqh.mongodb.net:27017/test?ssl=true&replicaSet=nodebc-shard-0&authSource=admin&retryWrites=true&w=majority';
+const Message = mongoose.model('Message', { name : String, message : String});
+mongoose.connect(dbUrl , (err) => { 
+  console.log('mongodb connected', err);
+})
 
 //middleware
 app.set('view engine', 'pug')
@@ -24,23 +32,25 @@ app.get('/home', home);
 app.get('/node', nodebcChat);
 app.get('/about', about);
 
-//io.io -> user connection console log
+//displays messages
+io.on('connection', (io)=>{
+  io.on('chat message', (msg)=>{
+    io.emit('chat message', msg);
+  });
+});
+
+//socket.io -> user connection console log
 io.on('connection', (io)=>{
   console.log('a user', ('connected'.green));
   io.on('disconnect', (io)=>{
     console.log('a user', ('disconnected'.red));
   });
 }); 
+
 //socket.io -> message console log
 io.on('connection', (io)=>{
   io.on('chat message', (msg)=>{
     console.log(('Message: '.blue) + msg.grey);
-  })
-})
-//displays messages
-io.on('connection', (io)=>{
-  io.on('chat message', (msg)=>{
-    io.emit('chat message', msg);
   });
 });
 
